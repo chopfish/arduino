b1ceb6bf7c56ef09f63a63bdcd4a2761673bd414
pkellym <mysterigirl1@gmail.com>
2014-12-06 12:17:36 -0500

    random lights

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,30 +1,46 @@
-#define LED 13
-#define BUTTON 7
+#define LED1 13
+#define BUTTON1 7
+#define LED2 12
+#define BUTTON2 6
+#define LED3 11
+#define BUTTON3 5
+#define LED4 10
+#define BUTTON4 4
+
 
 // the setup function runs once when you press reset or power the board
-void setup() {
-  // initialize digital pin 13 as an output.
-  pinMode(LED, OUTPUT);
-  pinMode(BUTTON, INPUT);
+void setup() 
+{
+  pinMode(LED1, OUTPUT);
+  pinMode(BUTTON1, INPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(BUTTON2, INPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(BUTTON3, INPUT);
+  pinMode(LED4, OUTPUT);
+  pinMode(BUTTON4, INPUT);
   
 }
 
 // the loop function runs over and over again forever
-void loop() {
-  //while(digitalRead(BUTTON)==HIGH)
-  //{
-    //digitalWrite(LED)=HIGH;
-  //}
-  //else
-    //digitalWrite(LED)=LOW;
-    
-  if(digitalRead(BUTTON)==HIGH)
+void loop() 
+{  
+  int x=(int)(random()*3+1); //1-4
+  if(x==1)
   {
-    digitalWrite(LED, HIGH);
+    digitalWrite(LED1,HIGH);
   }
-  else
+  else if(x==2)
   {
-    digitalWrite(LED, LOW);
+    digitalWrite(LED2,HIGH);
   }
-  delay(200);
+  else if(x==3)
+  {
+    digitalWrite(LED3,HIGH);
+  }
+  if(x==4)
+  {
+    digitalWrite(LED4,HIGH);
+  }
 }
+
