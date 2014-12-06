35e02e459ec19df699d3da94998d80edf40a3ad5
pkellym <mysterigirl1@gmail.com>
2014-12-06 11:42:21 -0500

    Button led

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,18 +1,5 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the Uno and
-  Leonardo, it is attached to digital pin 13. If you're unsure what
-  pin the on-board LED is connected to on your Arduino model, check
-  the documentation at http://arduino.cc
-
-  This example code is in the public domain.
-
-  modified 8 May 2014
-  by Scott Fitzgerald
- */
-
+#define LED 13
+#define BUTTON 7
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -24,10 +11,20 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  while(digitalRead(BUTTON)==HIGH)
+  //while(digitalRead(BUTTON)==HIGH)
+  //{
+    //digitalWrite(LED)=HIGH;
+  //}
+  //else
+    //digitalWrite(LED)=LOW;
+    
+  if(digitalRead(BUTTON)==HIGH)
   {
-    digitalWrite(LED)=HIGH;
+    digitalWrite(LED, HIGH);
   }
   else
-    digitalWrite(LED)=LOW;
+  {
+    digitalWrite(LED, LOW);
+  }
+  delay(200);
 }
