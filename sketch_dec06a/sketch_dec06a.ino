89c67e0a648d32851d307e9600b6ad2b875b4946
steve <zhikhe@gmail.com>
2014-12-06 11:06:16 -0500

    added stuff again

--- sketch_dec06a/sketch_dec06a.ino
+++ sketch_dec06a/sketch_dec06a.ino
@@ -1,12 +1,8 @@
 int led = 13;
-
-// the setup routine runs once when you press reset:
 void setup() {                
-  // initialize the digital pin as an output.
   pinMode(led, OUTPUT);     
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);               // wait for a second
