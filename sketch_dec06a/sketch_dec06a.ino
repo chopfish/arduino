cc82d818fccc1009fce61ef618ab75729b1d57fb
steve <zhikhe@gmail.com>
2014-12-06 12:35:10 -0500

    added hackthis

--- sketch_dec06a/sketch_dec06a.ino
+++ sketch_dec06a/sketch_dec06a.ino
@@ -1,17 +1,53 @@
-int ledPin = 13; // choose the pin for the LED
-int inPin = 7;   // choose the input pin (for a pushbutton)
-int val = 0;     // variable for reading the pin status
+int led = 13; // choose the pin for the LED
+int button = 7;// choose the input pin (for a pushbutton)
+int led2 = 12;
+int button2 = 6;
+int led3 = 11;
+int button3 = 5;
+int led4 = 11;
+int button4 = 4;
 
-void setup() {
-  pinMode(ledPin, OUTPUT);  // declare LED as output
-  pinMode(inPin, INPUT);    // declare pushbutton as input
+// the setup function runs once when you press reset or power the board
+void setup() 
+{
+  pinMode(led, OUTPUT);
+  pinMode(button, INPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(button2, INPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(button3, INPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(button4, INPUT);
+  
 }
 
-void loop(){
-  val = digitalRead(inPin);  // read input value
-  if (val == HIGH) {         // check if the input is HIGH (button released)
-    digitalWrite(ledPin, LOW);  // turn LED OFF
-  } else {
-    digitalWrite(ledPin, HIGH);  // turn LED ON
+// the loop function runs over and over again forever
+void loop() 
+{  
+  int x=(int)(random()*3+1); //1-4
+  if(x==1)
+  {
+    digitalWrite(LED1,HIGH);
+    delay(500);
+    digitalWrite(LED1, LOW);
+  }
+  else if(x==2)
+  {
+    digitalWrite(LED2,HIGH);
+    delay(500);
+    digitalWrite(LED2, LOW);
+  }
+  else if(x==3)
+  {
+    digitalWrite(LED3,HIGH);
+    delay(500);
+    digitalWrite(LED3, LOW);
+  }
+  if(x==4)
+  {
+    digitalWrite(LED4,HIGH);
+    delay(500);
+    digitalWrite(LED4, LOW);
   }
 }
+
